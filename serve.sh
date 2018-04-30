590664ea511bafe628440d81a45a83c4cb55c2aa
hadihariri <mail@hadihariri.com>
2018-04-30 11:56:38 +0200

    Fixed date on post

--- serve.sh
+++ serve.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -v /Users/hadihariri/Documents/Web/hhariri.github.io:/tmp -p 4000:4000 hhariri/jekyll jekyll serve -s /tmp --host 0.0.0.0 --incremental
+docker run -v /Users/hadihariri/Documents/Web/hhariri.github.io:/tmp -p 4000:4000 jekyll jekyll serve -s /tmp --host 0.0.0.0 --incremental
